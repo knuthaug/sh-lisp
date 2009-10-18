8a129cc61103692612d21688fb93c0ac26f0ff7e
Knut Haugen <knuthaug+github@gmail.com>
2009-10-18 09:38:20 +0200

    added javascript brush to compress script

--- compress.sh
+++ compress.sh
@@ -4,7 +4,7 @@ cd scripts;
 yui="java -jar ../yuicompressor-2.4.2.jar --nomunge -o tmp"
 
 
-for name in shCore.js shBrushCSharp.js shBrushCss.js shBrushBash.js shBrushDiff.js shBrushJava.js shBrushPerl.js shBrushPhp.js shBrushPlain.js shBrushPython.js shBrushRuby.js shBrushSql.js shBrushXml.js shBrushYaml.js
+for name in shCore.js shBrushCSharp.js shBrushCss.js shBrushBash.js shBrushDiff.js shBrushJava.js shBrushPerl.js shBrushPhp.js shBrushPlain.js shBrushPython.js shBrushRuby.js shBrushSql.js shBrushXml.js shBrushYaml.js shBrushLisp.js shBrushJScript.js
 do 
     $yui --type js $name 
     cat tmp >> sh.js
